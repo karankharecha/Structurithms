0060eb69a692622c3284c47e6f36c2072f7b900a
Karan Kharecha <kharechakaran67@gmail.com>
2019-01-06 19:28:55 +0530

    Insertion sort added

--- src/com/structurithms/VintageSorts.java
+++ src/com/structurithms/VintageSorts.java
@@ -1,119 +1,95 @@
 package com.structurithms;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class VintageSorts {
 
-    public static <T> void mergeSort(List<T> list, Comparator<T> comparator){
+    public static <T> void mergeSort(List<T> list, Comparator<T> comparator) {
         Object[] array = list.toArray();
-        mergeSortOperation(array,0, array.length-1, comparator);
-        for (int i=0;i<array.length;i++){
-            list.set(i, (T)array[i]);
+        mergeSortOperation(array, 0, array.length - 1, comparator);
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
         }
     }
 
-    public static <T> void mergeSort(List<T> list){
+    public static <T> void mergeSort(List<T> list) {
         Object[] array = list.toArray();
-        mergeSortOperation(array,0, array.length-1);
-        for (int i=0;i<array.length;i++){
-            list.set(i, (T)array[i]);
+        mergeSortOperation(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
         }
     }
 
     public static <T> void quickSort(List<T> list, Comparator<T> comparator) {
         Object[] array = list.toArray();
-        quickSortOperation(array, 0, array.length-1, comparator);
-        for (int i=0;i<array.length;i++){
-            list.set(i, (T)array[i]);
+        quickSortOperation(array, 0, array.length - 1, comparator);
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
         }
     }
 
     public static <T> void quickSort(List<T> list) {
         Object[] array = list.toArray();
-        quickSortOperation(array, 0, array.length-1);
-        for (int i=0;i<array.length;i++){
-            list.set(i, (T)array[i]);
+        quickSortOperation(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
         }
     }
 
     public static <T> void heapSort(List<T> list, Comparator<T> comparator) {
         Object[] array = list.toArray();
         int n = array.length;
-        for(int i=(n/2)-1 ; i>=0 ; i--){
-            heapify(array,i,n, comparator);
+        for (int i = (n / 2) - 1; i >= 0; i--) {
+            heapify(array, i, n, comparator);
         }
-        for(int i=(n-1) ; i>=0 ; i--){
+        for (int i = (n - 1); i >= 0; i--) {
             Object temp = array[0];
             array[0] = array[i];
             array[i] = temp;
-            heapify(array,0,i, comparator);
+            heapify(array, 0, i, comparator);
         }
-        for (int i=0;i<array.length;i++){
-            list.set(i, (T)array[i]);
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
         }
     }
 
     public static <T> void heapSort(List<T> list) {
         Object[] array = list.toArray();
         int n = array.length;
-        for(int i=(n/2)-1 ; i>=0 ; i--){
-            heapify(array,i,n);
+        for (int i = (n / 2) - 1; i >= 0; i--) {
+            heapify(array, i, n);
         }
-        for(int i=(n-1) ; i>=0 ; i--){
+        for (int i = (n - 1); i >= 0; i--) {
             Object temp = array[0];
             array[0] = array[i];
             array[i] = temp;
-            heapify(array,0,i);
+            heapify(array, 0, i);
         }
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
+        }
+    }
+
+    public static <T> void insertionSort(List<T> list, Comparator<T> comparator){
+        Object[] array = list.toArray();
+        insertionSortOperation(array, comparator);
+        for (int i = 0; i < array.length; i++) {
+            list.set(i, (T) array[i]);
+        }
+    }
+
+    public static <T> void insertionSort(List<T> list) {
+        Object[] array = list.toArray();
+        insertionSortOperation(array);
         for (int i=0;i<array.length;i++){
             list.set(i, (T)array[i]);
         }
     }
 
-    private static <T> void quickSortOperation(Object[] arr, int first, int last, Comparator<T> comparator){
-        if(first < last){
-            Object pivot = arr[last];
-            int i = (first - 1);
-            for (int j=first;j<last;j++){
-                if(comparator.compare((T)arr[j], (T)pivot) <= 0){
-                    Object temp = arr[++i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-            Object temp = arr[++i];
-            arr[i] = arr[last];
-            arr[last] = temp;
 
-            quickSortOperation(arr, first, i-1, comparator);
-            quickSortOperation(arr, i+1, last, comparator);
-        }
-    }
-
-    private static void quickSortOperation(Object[] arr, int first, int last){
-        if(first < last){
-            Object pivot = arr[last];
-            int i = (first - 1);
-            for (int j=first;j<last;j++){
-                if(((Comparable)arr[j]).compareTo(pivot) <= 0){
-                    Object temp = arr[++i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-            Object temp = arr[++i];
-            arr[i] = arr[last];
-            arr[last] = temp;
-
-            quickSortOperation(arr, first, i-1);
-            quickSortOperation(arr, i+1, last);
-        }
-    }
-
-    private static <T> void mergeSortOperation(Object[] arr, int first, int last, Comparator<T> comparator){
-        if(first < last){
+    private static <T> void mergeSortOperation(Object[] arr, int first, int last, Comparator<T> comparator) {
+        if (first < last) {
             int mid = (first + last) >>> 1;
             mergeSortOperation(arr, first, mid, comparator);
             mergeSortOperation(arr, mid + 1, last, comparator);
@@ -122,26 +98,26 @@ public class VintageSorts {
             Object[] leftArray = new Object[s1];
             Object[] rightArray = new Object[s2];
             System.arraycopy(arr, first, leftArray, 0, s1);
-            System.arraycopy(arr,mid+1, rightArray, 0, s2);
-            int i,j,k;
-            for(i=0, j=0, k=first; i<s1 && j<s2; k++){
-                if (comparator.compare((T)leftArray[i], (T)rightArray[j]) <= 0){
+            System.arraycopy(arr, mid + 1, rightArray, 0, s2);
+            int i, j, k;
+            for (i = 0, j = 0, k = first; i < s1 && j < s2; k++) {
+                if (comparator.compare((T) leftArray[i], (T) rightArray[j]) <= 0) {
                     arr[k] = leftArray[i++];
                 } else {
                     arr[k] = rightArray[j++];
                 }
             }
-            if (i < s1){
-                System.arraycopy(leftArray, i, arr, k, s1-i);
+            if (i < s1) {
+                System.arraycopy(leftArray, i, arr, k, s1 - i);
             }
-            if (j < s2){
-                System.arraycopy(rightArray, j, arr, k, s2-j);
+            if (j < s2) {
+                System.arraycopy(rightArray, j, arr, k, s2 - j);
             }
         }
     }
 
-    private static void mergeSortOperation(Object[] arr, int first, int last){
-        if(first < last){
+    private static void mergeSortOperation(Object[] arr, int first, int last) {
+        if (first < last) {
             int mid = (first + last) >>> 1;
             mergeSortOperation(arr, first, mid);
             mergeSortOperation(arr, mid + 1, last);
@@ -150,57 +126,121 @@ public class VintageSorts {
             Object[] leftArray = new Object[s1];
             Object[] rightArray = new Object[s2];
             System.arraycopy(arr, first, leftArray, 0, s1);
-            System.arraycopy(arr,mid+1, rightArray, 0, s2);
-            int i,j,k;
-            for(i=0, j=0, k=first; i<s1 && j<s2; k++){
-                if (((Comparable)leftArray[i]).compareTo(rightArray[j]) <= 0){
+            System.arraycopy(arr, mid + 1, rightArray, 0, s2);
+            int i, j, k;
+            for (i = 0, j = 0, k = first; i < s1 && j < s2; k++) {
+                if (((Comparable) leftArray[i]).compareTo(rightArray[j]) <= 0) {
                     arr[k] = leftArray[i++];
                 } else {
                     arr[k] = rightArray[j++];
                 }
             }
-            if (i < s1){
-                System.arraycopy(leftArray, i, arr, k, s1-i);
+            if (i < s1) {
+                System.arraycopy(leftArray, i, arr, k, s1 - i);
             }
-            if (j < s2){
-                System.arraycopy(rightArray, j, arr, k, s2-j);
+            if (j < s2) {
+                System.arraycopy(rightArray, j, arr, k, s2 - j);
             }
         }
     }
 
-    private static <T> void heapify(Object[] arr, int first, int last, Comparator<T> comparator){
-        int index = first;
-        int leftIndex = (index*2) + 1;
-        int rightIndex = (index*2) + 2;
-        if(leftIndex < last && comparator.compare((T)arr[leftIndex], (T)arr[index]) > 0){
-            index = leftIndex;
-        }
-        if(rightIndex < last && comparator.compare((T)arr[rightIndex], (T)arr[index]) > 0){
-            index = rightIndex;
-        }
-        if( index != first ){
-            Object temp = arr[index];
-            arr[index] = arr[first];
-            arr[first] = temp;
-            heapify(arr,index,last, comparator);
+    private static <T> void quickSortOperation(Object[] arr, int first, int last, Comparator<T> comparator) {
+        if (first < last) {
+            Object pivot = arr[last];
+            int i = (first - 1);
+            for (int j = first; j < last; j++) {
+                if (comparator.compare((T) arr[j], (T) pivot) <= 0) {
+                    Object temp = arr[++i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            Object temp = arr[++i];
+            arr[i] = arr[last];
+            arr[last] = temp;
+
+            quickSortOperation(arr, first, i - 1, comparator);
+            quickSortOperation(arr, i + 1, last, comparator);
         }
     }
 
-    private static void heapify(Object[] arr, int first, int last){
+    private static void quickSortOperation(Object[] arr, int first, int last) {
+        if (first < last) {
+            Object pivot = arr[last];
+            int i = (first - 1);
+            for (int j = first; j < last; j++) {
+                if (((Comparable) arr[j]).compareTo(pivot) <= 0) {
+                    Object temp = arr[++i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            Object temp = arr[++i];
+            arr[i] = arr[last];
+            arr[last] = temp;
+
+            quickSortOperation(arr, first, i - 1);
+            quickSortOperation(arr, i + 1, last);
+        }
+    }
+
+    private static <T> void heapify(Object[] arr, int first, int last, Comparator<T> comparator) {
         int index = first;
-        int leftIndex = (index*2) + 1;
-        int rightIndex = (index*2) + 2;
-        if(leftIndex < last && ((Comparable)arr[leftIndex]).compareTo(arr[index]) > 0){
+        int leftIndex = (index * 2) + 1;
+        int rightIndex = (index * 2) + 2;
+        if (leftIndex < last && comparator.compare((T) arr[leftIndex], (T) arr[index]) > 0) {
             index = leftIndex;
         }
-        if(rightIndex < last && ((Comparable)arr[rightIndex]).compareTo(arr[index]) > 0){
+        if (rightIndex < last && comparator.compare((T) arr[rightIndex], (T) arr[index]) > 0) {
             index = rightIndex;
         }
-        if( index != first){
+        if (index != first) {
             Object temp = arr[index];
             arr[index] = arr[first];
             arr[first] = temp;
-            heapify(arr,index,last);
+            heapify(arr, index, last, comparator);
+        }
+    }
+
+    private static void heapify(Object[] arr, int first, int last) {
+        int index = first;
+        int leftIndex = (index * 2) + 1;
+        int rightIndex = (index * 2) + 2;
+        if (leftIndex < last && ((Comparable) arr[leftIndex]).compareTo(arr[index]) > 0) {
+            index = leftIndex;
+        }
+        if (rightIndex < last && ((Comparable) arr[rightIndex]).compareTo(arr[index]) > 0) {
+            index = rightIndex;
+        }
+        if (index != first) {
+            Object temp = arr[index];
+            arr[index] = arr[first];
+            arr[first] = temp;
+            heapify(arr, index, last);
+        }
+    }
+
+    private static <T> void insertionSortOperation(Object[] arr, Comparator<T> comparator){
+        for (int i = 1; i <arr.length; ++i) {
+            T key = (T) arr[i];
+            int j = i - 1;
+            while (j >= 0 && (comparator.compare((T)arr[j], key)) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    private static <T> void insertionSortOperation(Object[] arr) {
+        for (int i = 1; i <arr.length; ++i) {
+            T key = (T) arr[i];
+            int j = i - 1;
+            while (j >= 0 && ((Comparable)arr[j]).compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
     }
 
